82ba63403ed4a73bb73da15386486b986a0d5220
Facundo Esteban Lemo <facundolemo@gmail.com>
2023-07-07 18:24:36 -0300

    Posting feature now working w/o usernor comment

--- app.py
+++ app.py
@@ -39,10 +39,12 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(70), nullable=False)
     content = db.Column(db.String(500), nullable=True)
-    time_created = db.Column(DateTime(timezone=True), server_default=func.now())
+    time_created = db.Column(DateTime(timezone=True),
+                              server_default=func.now())
     time_updated = db.Column(DateTime(timezone=True), onupdate=func.now())
     user_id = db.Column(db.Integer, ForeignKey("user.id"), nullable=False)
-    category_id = db.Column(db.Integer, ForeignKey("category.id"), nullable=True)
+    category_id = db.Column(db.Integer, ForeignKey("category.id"),
+                             nullable=True)
 
     def __str__(self):
         return f"-Category: f{self.name}"
@@ -51,14 +53,47 @@ class Comment(db.Model):
     __tablename__ = "comment"
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    time_created = db.Column(DateTime(timezone=True), server_default=func.now())
+    time_created = db.Column(DateTime(timezone=True),
+                              server_default=func.now())
     time_updated = db.Column(DateTime(timezone=True), onupdate=func.now())
     user_id = db.Column(db.Integer, ForeignKey("user.id"), nullable=False)
 
     def __str__(self):
         return f"-Comment '{self.content}' by {self.user_id}"
 
+#TODO: CONTEXT PROCESSOR PASA EL ID DEL USER SELECCIONADO
+#CADA FUNCION TOMA EL ID COMO PARAMETRO PARA SABER Q USER SUBIO EL POST/COMENTARIO
+@app.context_processor
+def inject_paises():
+    posts=db.session.query(Post).all()
+    return dict(posts=posts)
 
 @app.route("/")
 def Index():
     return render_template('index.html')
+
+@app.route("/add_post", methods=['POST'])
+def AddPost():
+    if request.method=='POST':
+        title = request.form['title']
+        content = request.form['content']
+        category = request.form['category']
+        #obtener el user
+        user=1
+        new_post=Post(title=title,content=content,category_id=category,
+                      user_id=user)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('Index'))
+
+@app.route("/add_comment", methods=['POST'])
+def AddComment():
+    if request.method=='POST':
+        content = request.form['content']
+        new_comment=Comment(content=nombre_pais)
+        db.session.add(nuevo_pais)
+        db.session.commit()
+
+        return redirect(url_for('Index'))
+
